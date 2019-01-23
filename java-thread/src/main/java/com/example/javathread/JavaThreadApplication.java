39856fc443f6616db2f6a36710de382de29f7413
jianganwei <2550889007@qq.com>
2019-01-23 18:26:03 +0800

    demo2

---
+++ java-thread/src/main/java/com/example/javathread/JavaThreadApplication.java
@@ -0,0 +1,14 @@
+package com.example.javathread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaThreadApplication.class, args);
+    }
+
+}
+
